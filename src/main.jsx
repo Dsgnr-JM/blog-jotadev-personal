cbb8bb71539343fb83aa3ca6e794893bcc4bd97c
JotaDev <mejiajhoan2021@gmail.com>
2024-03-17 22:33:20 -0430

    Add vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 import '@fontsource-variable/onest'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter>
+    <Analytics/>
     <App />
   </HashRouter>
 )
