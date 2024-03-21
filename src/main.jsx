3edc42c0fea42e5fbc97704ce63e590cfb88feb1
Jhoan Mejia <126351655+Dsgnr-JM@users.noreply.github.com>
2024-03-21 09:48:47 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import '@fontsource-variable/onest'
 import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Analytics/>
     <App />
   </BrowserRouter>
