773695d3c080c1f5e5ee01a926bf395b5b9c34ba
JotaDev <mejiajhoan2021@gmail.com>
2024-03-27 13:05:42 -0430

    Add component Background

---
+++ src/components/Background.jsx
@@ -0,0 +1,8 @@
+export default function Background() {
+  return (
+    <>
+      <div className="fixed top-2/3 -translate-y-1/2 md:-left-[20%] -left-[50%] h-[31rem] w-[31rem] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(74,172,227,.08),transparent)] dark:bg-[radial-gradient(circle_farthest-side,rgba(74,172,227,.1),transparent)]" />
+      <div className="fixed top-1/3 -translate-y-1/2 md:right-0 -right-[50%] h-[31rem] w-[31rem] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(246,131,240,.08),transparent)] dark:bg-[radial-gradient(circle_farthest-side,rgba(246,131,240,.12),transparent)]" />
+    </>
+  )
+}
